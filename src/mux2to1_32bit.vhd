47eec89a1df8d5151a5f162b5cfcfb2d084530ac
Mohamad Ashraf Salama <mohamadashrafaast@gmail.com>
2023-05-03 20:15:32 +0000

    add 32-bit 2-to-1 multiplexer

---
+++ src/mux2to1_32bit.vhd
@@ -0,0 +1,25 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Mux2to1_32bit is
+    Port (
+        a      : in  STD_LOGIC_VECTOR(31 downto 0);
+        b      : in  STD_LOGIC_VECTOR(31 downto 0);
+        sel    : in  STD_LOGIC;
+        output : out STD_LOGIC_VECTOR(31 downto 0)
+    );
+end Mux2to1_32bit;
+
+architecture Behavioral of Mux2to1_32bit is
+begin
+
+    process(a, b, sel)
+    begin
+        if sel = '0' then
+            output <= a;
+        else
+            output <= b;
+        end if;
+    end process;
+
+end Behavioral;
